70435916963cec88f50febe6851ec89477e0e1af
Fraju-pc <patrick.f.corcoran@gmail.com>
2023-10-12 12:24:30 -0400

    Week 7 Curricula done

---
+++ Assignments/Week07/mysql-java/src/main/java/recipies/recipes.java
@@ -0,0 +1,14 @@
+package recipies;
+
+import java.sql.Connection;
+import recipies.dao.DbConnection;
+
+public class recipes {
+
+	public static void main(String[] args) {
+
+		//DbConnection.getConnection();
+
+	}
+
+}
